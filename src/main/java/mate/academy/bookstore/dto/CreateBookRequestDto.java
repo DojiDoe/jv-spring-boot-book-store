42635b25f091648856a552106ed35f59b9009f1d
Sasha <sasha.panchyshun@gmail.com>
2024-09-26 13:53:58 +0300

    spring-boot-globalExceptionHandler-and-data-validation-solution2

--- src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
@@ -2,23 +2,28 @@ package mate.academy.bookstore.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
-import mate.academy.bookstore.validation.Isbn;
+import org.hibernate.validator.constraints.ISBN;
 
 @Getter
 @Setter
 public class CreateBookRequestDto {
     @NotNull
+    @Pattern(regexp = "^(?=\\s*\\S).*$")
     private String title;
     @NotNull
+    @Pattern(regexp = "^(?=\\s*\\S).*$")
     private String author;
-    @Isbn
+    @ISBN
     private String isbn;
     @Min(0)
     private BigDecimal price;
     @NotNull
+    @Size(min = 10, max = 255)
     private String description;
     @NotNull
     private String coverImage;
