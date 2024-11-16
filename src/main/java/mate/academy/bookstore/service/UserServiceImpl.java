d6b0b38957cfeb3f6de4c03a20938fde2a45c867
Sasha <sasha.panchyshun@gmail.com>
2024-11-16 19:17:05 +0200

    spring-boot-security1

--- src/main/java/mate/academy/bookstore/service/UserServiceImpl.java
+++ src/main/java/mate/academy/bookstore/service/UserServiceImpl.java
@@ -1,12 +1,16 @@
 package mate.academy.bookstore.service;
 
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstore.dto.user.UserRegistrationRequestDto;
 import mate.academy.bookstore.dto.user.UserResponseDto;
 import mate.academy.bookstore.exception.RegistrationException;
 import mate.academy.bookstore.mapper.UserMapper;
+import mate.academy.bookstore.model.Role;
 import mate.academy.bookstore.model.User;
+import mate.academy.bookstore.repository.RoleRepository;
 import mate.academy.bookstore.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,13 +18,17 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto) {
         if (userRepository.existsByEmail(requestDto.getEmail())) {
             throw new RegistrationException("Can't register user");
         }
+        requestDto.setPassword(passwordEncoder.encode(requestDto.getPassword()));
         User user = userMapper.toModel(requestDto);
+        user.setRoles(Set.of(roleRepository.getRoleByRole(Role.RoleName.ROLE_USER)));
         userRepository.save(user);
         return userMapper.toDto(user);
     }
